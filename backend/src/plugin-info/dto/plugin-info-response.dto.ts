1be88ec9875d1be5046d640865817e3bc7752921
Pauline Banye <paulinebanye@gmail.com>
2026-01-02 18:22:22 +0000

    docs: annotate plugin info dto for swagger

--- backend/src/plugin-info/dto/plugin-info-response.dto.ts
+++ backend/src/plugin-info/dto/plugin-info-response.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
 import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
 
 class PluginInfoDetailsDto {
+  @ApiProperty()
   @Expose()
   @IsString()
   name!: string;
 
+  @ApiProperty({ type: [String] })
   @Expose()
   @IsArray()
   @IsString({ each: true })
@@ -13,64 +16,77 @@ class PluginInfoDetailsDto {
 }
 
 class PluginInfoDataDto {
+  @ApiProperty()
   @Expose()
   @IsString()
   type!: string;
 
+  @ApiProperty({ type: () => PluginInfoDetailsDto })
   @Expose()
   @ValidateNested()
   @Type(() => PluginInfoDetailsDto)
   plugin_info!: PluginInfoDetailsDto;
 
+  @ApiProperty()
   @Expose()
   @IsString()
   version!: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   scaffold_structure?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   team?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   developer_name?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   developer_email?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   icon_url?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   photos?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   homepage_url?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   sidebar_url?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
   install_url?: string;
 
+  @ApiProperty({ required: false })
   @Expose()
   @IsOptional()
   @IsString()
@@ -78,15 +94,18 @@ class PluginInfoDataDto {
 }
 
 export class PluginInfoResponseDto {
+  @ApiProperty()
   @Expose()
   @IsString()
   message!: string;
 
+  @ApiProperty({ type: () => PluginInfoDataDto })
   @Expose()
   @ValidateNested()
   @Type(() => PluginInfoDataDto)
   data!: PluginInfoDataDto;
 
+  @ApiProperty()
   @Expose()
   @IsString()
   success!: string;
