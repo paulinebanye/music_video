4f5edda7afb1a40fdf759c6c213a06e877461d58
Pauline Banye <paulinebanye@gmail.com>
2026-01-02 14:40:37 +0000

    feat: wire postgres datasource for typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PluginInfoModule } from './plugin-info/plugin-info.module';
 import { CommentModule } from './comments/comment.module';
 import { LoggerModule } from './common/logger/logger.module';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [LoggerModule, PluginInfoModule, CommentModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    LoggerModule,
+    PluginInfoModule,
+    CommentModule,
+  ],
 })
 export class AppModule {}
