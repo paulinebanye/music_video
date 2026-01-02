dfad4743b8dc0efda66925bec42dc4d82e258a10
Pauline Banye <paulinebanye@gmail.com>
2026-01-02 11:30:27 +0000

    refactor: register logger module in app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PluginInfoModule } from './plugin-info/plugin-info.module';
 import { CommentModule } from './comments/comment.module';
+import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
-  imports: [PluginInfoModule, CommentModule],
+  imports: [LoggerModule, PluginInfoModule, CommentModule],
 })
 export class AppModule {}
